c16c6943ff00466fe0a2d7cc28f3319d72ba80f3
Ihdan <maghikhdan@gmail.com>
2023-10-29 10:51:48 +0700

    fixed file path header

--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,6 @@
 import Layout from "./Layout";
 import Category from "./Category";
-import Header from "./Header";
+import Header from "./header";
 import Products from "./products";
 import Skeleton from "./Skeleton";
 export { Layout, Category, Header, Products, Skeleton };
